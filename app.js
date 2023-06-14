08ac79095930f325cf2d52751bb53caee24fc124
Webdevava <115859079+Webdevava@users.noreply.github.com>
2023-06-14 06:23:27 +0000

    fixed register and login

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://ankurauti:ankur382@cluster0.90pwhsy.mongodb.net/?retryWrites=true&w=majority/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  try {
+    await newUser.save();
+    res.redirect("/secrets");
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const foundUser = await User.findOne({ email: username });
+
+    if (foundUser && foundUser.password === password) {
+      res.redirect("/secrets");
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+app.get("/secrets", (req, res) => {
+  res.render("secrets");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
